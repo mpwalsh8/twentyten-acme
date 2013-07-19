c0fa69c6e33e3745e2833a1405fbeb8cfe24b98c
mpwalsh8 <mpwalsh8@gmail.com>
2013-07-19 02:46:39 +0100

    Added 'Shop Fulfillment' role to allow a user to process placed orders.

--- xtra/woocommerce.php
+++ xtra/woocommerce.php
@@ -30,4 +30,63 @@ function acme_loop_shop_columns()
 {
     return 3;
 }
+
+/**
+ * Need a role for WooCommerce Shop Fulfillment
+ *
+ */
+function acme_add_roles()
+{
+    //  List of WooCommerce capabilities needed for fullfillment
+    //  based on email from WC Support.  Some have been set to false
+    //  to prevent certain operations.
+
+    $acme_wc_fullfillment_role = new WP_Role('acme_shop_fullfillment', array(
+        'read' => true,
+        'edit_shop_order' => true,
+        'read_shop_order' => true,
+        'delete_shop_order' => false,
+        'edit_shop_orders' => true,
+        'edit_others_shop_orders' => true,
+        'publish_shop_orders' => false,
+        'read_private_shop_orders' => true,
+        'delete_shop_orders' => false,
+        'delete_private_shop_orders' => false,
+        'delete_published_shop_orders' => false,
+        'delete_others_shop_orders' => false,
+        'edit_private_shop_orders' => true,
+        'edit_published_shop_orders' => true,
+        'manage_shop_order_terms' => false,
+        'edit_shop_order_terms' => false,
+        'delete_shop_order_terms' => false,
+        'assign_shop_order_terms' => false,
+        'view_woocommerce_reports' => true,
+    ));
+
+    //  If the role already exists, test it to see if any capability
+    //  has been changed.  If it has, the role needs to be removed and
+    //  then added again with the new capabilities.
+
+    $role = get_role($acme_wc_fullfillment_role->name);
+
+    //  Sort the data by keys so the array can be compared
+
+    ksort($role->capabilities) ;
+    ksort($acme_wc_fullfillment_role->capabilities) ;
+
+    //  If what is stored doesn't match what the default is, delete it and add it again
+
+    if ($role->capabilities != $acme_wc_fullfillment_role->capabilities)
+    {
+        $role = remove_role($acme_wc_fullfillment_role->name);
+
+        $role = add_role($acme_wc_fullfillment_role->name,
+            __('Shop Fullfillment', 'woocommerce'), $acme_wc_fullfillment_role->capabilities) ;
+    }
+}
+
+/**
+ * Initiate the action to add roles after the theme is set up
+ **/
+add_action('after_setup_theme','acme_add_roles');
 ?>
